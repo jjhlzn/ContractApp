8914caa38613fc860c52cf839e5a181ca86947f2
jjhlzn <jinjunhang@hotmail.com>
2016-04-24 15:54:41 +0800

    优化个人信息页面

---
+++ ContractApp/MyInfoMainCell.swift
@@ -0,0 +1,15 @@
+//
+//  MyInfoMainCell.swift
+//  ContractApp
+//
+//  Created by 刘兆娜 on 16/4/24.
+//  Copyright © 2016年 金军航. All rights reserved.
+//
+
+import UIKit
+
+class MyInfoMainCell: UITableViewCell {
+
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userImageView: UIImageView!
+}
--- ContractApp/MyInfoViewController.swift
+++ ContractApp/MyInfoViewController.swift
@@ -22,46 +22,67 @@ class MyInfoViewController: BaseUIViewController, UITableViewDataSource, UITable
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 2
+        return 3
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return 3
-        } else {
+        switch section {
+        case 0:
             return 1
+        case 1:
+            return 3
+        case 2:
+            return 1
+        default:
+            return 0
         }
-        
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        let section = indexPath.section
+        switch section {
+        case 0:
+            return 153
+        case 1:
+            return 44
+        case 2:
+            return 71
+        default:
+            return 0
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let row = indexPath.row
         let section = indexPath.section
-        
         if section == 0 {
+            let cell = tableView.dequeueReusableCellWithIdentifier("myInfoMainCell") as! MyInfoMainCell
+            cell.userNameLabel.text = loginUser.name
+            return cell
+        } else if section == 1 {
             if row == 0 {
-                let cell = tableView.dequeueReusableCellWithIdentifier("myInfoCell") as! MyInfoCell
-                cell.nameLabel.text = "用户名"
+                let cell = tableView.dequeueReusableCellWithIdentifier("myInfoOtherCell") as! MyInfoCell
+                cell.nameLabel.text = "帐号"
                 cell.valueLabel.text = loginUser.userName
                 return cell
             } else if row == 1 {
-                let cell = tableView.dequeueReusableCellWithIdentifier("myInfoCell") as! MyInfoCell
-                cell.nameLabel.text = "姓名"
-                cell.valueLabel.text = loginUser.name
-                return cell
-            } else {
-                let cell = tableView.dequeueReusableCellWithIdentifier("myInfoCell") as! MyInfoCell
+                let cell = tableView.dequeueReusableCellWithIdentifier("myInfoOtherCell") as! MyInfoCell
                 cell.nameLabel.text = "部门"
                 cell.valueLabel.text = loginUser.department
                 return cell
+            } else  {
+                let cell = tableView.dequeueReusableCellWithIdentifier("myInfoOtherCell") as! MyInfoCell
+                cell.nameLabel.text = "版本号"
+                cell.valueLabel.text = "1.0"
+                return cell
+
             }
         }
         else {
             let cell = tableView.dequeueReusableCellWithIdentifier("logoutCell")!
-            cell.backgroundColor = tableView.backgroundColor
+            //cell.backgroundColor = tableView.backgroundColor
             return cell
         }
-    
     }
 
     @IBAction func logoutPressed(sender: AnyObject) {
